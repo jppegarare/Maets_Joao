0889680c33ed1df1f9b1d8a52997542c37d863b0
João Pegarare <jppegarare18@gmail.com>
2024-04-19 08:45:44 -0400

    arrumando o formulario

--- index.js
+++ index.js
@@ -3,37 +3,37 @@ const conn = require("./db/conn");
 
 const Usuario = require("./models/Usuario")
 
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.use(
-//     express.urlencoded({
-//         extended: true,
-//     })
-// );
-// app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json())
 
-// app.get("/usuario/novo", (req, res) => {
-//     res.sendFile(`${__dirname}/views/formUsuario.html`)
-// })
+app.get("/usuarios/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
 
-// app.post("/usuario/novo", async (req, res) => {
-//     const nickname = req.body.nickname;
-//     const nome = req.body.nome;
+app.post("/usuarios/novo", async (req, res) => {
+    const nickname = req.body.nickname;
+    const nome = req.body.nome;
 
-//     const dadosUsuario = {
-//         nickname,
-//         nome,
-//     };
+    const dadosUsuario = {
+        nickname,
+        nome,
+    };
 
-//     const usuario = await Usuario.create(dadosUsuario)
+    const usuario = await Usuario.create(dadosUsuario)
 
-//     res.send("Usuario inserido sob o id " + usuario.id)
-// });
+    res.send("Usuario inserido sob o id " + usuario.id)
+});
 
-// app.listen(8000, () =>{
-//     console.log("Server rodadndo na porta 8000")
-// })
+app.listen(8000, () =>{
+    console.log("Server rodanddo na porta 8000")
+})
 conn
 .sync()
 .then(() => {
