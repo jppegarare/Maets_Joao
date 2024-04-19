a25c7584ddcdd84fdbeccc2d67b18757489a40c5
João Pegarare <jppegarare18@gmail.com>
2024-04-19 08:07:32 -0400

    finalizando o model usuario e adicionando get e post

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 
+const Usuario = require("./models/Usuario")
+
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json())
+
+app.get("/usuario/novo", (req, res) => {
+    res.sendFile(`${__dirname}/views/formUsuario.html`)
+})
+
+app.post("/usuario/novo", async (req, res) => {
+    const nickname = req.body.nickname;
+    const nome = req.body.nome;
+
+    const dadosUsuario = {
+        nickname,
+        nome,
+    };
+
+    const usuario = await Usuario.create(dadosUsuario)
+
+    res.send("Usuario inserido sob o id " + usuario.id)
+});
+
+app.listen(8000, () =>{
+    console.log("Server rodadndo na porta 8000")
+})
 conn
-.authenticate()
+.sync()
 .then(() => {
-    console.log("Parabéns, você está conectado ao Banco de Dados!")
+    console.log("Parabéns, você está conectado ao Banco de Dados e com a estrutura sincronizada!")
 })
 .catch((err) => {   
-    console.log("Eita!!! Parece que ocorreu o seguinte erro: " + err)
+    console.log("Erro ao conecyar/ sincronizar o bd: " + err)
 })
